a5a04f491db5cfb5121d39be4c6a3e86cae479fa
Rkaur <rupinder88uiet@gmail.com>
2021-04-20 01:16:13 -0400

    for Django server

---
+++ Final/predict/diseaseIndex.py
@@ -0,0 +1,117 @@
+'''disease_index = ("Apple with Apple_scab ",
+
+	"Apple with Black Rot",
+	"Apple with Cedar apple rust",
+	"healthy Apples","random background image",
+	"healthy Blueberry",
+	"healthy Cherry",
+	"Cherry with Powdery Mildew",
+	"Corn with Cercospora leaf spot",
+	"Corn with Common rust",
+	"healthy Corn",
+	"Northern Leaf Blight in Corn",
+	"Black Rot in Grape",
+	"Black Measels in Grape",
+	"healthy Grape",
+	"Isariopsis Leaf Spot in Grape",
+	"Citrus greening in Orange",
+	"Bacterial Spot in Peach",
+	"healthy Peach",
+	"Pepper with Bacterial Spot",
+	"Healthy Pepper",
+	"Potato Early Blight",
+	"Healthy Potato",
+	"Potato with Late Blight",
+	"Healthy Raspberry",
+	"healthy Soyabean",
+	"Powdery Mildew in Squash",
+	"Healthy Strawberry",
+	"Strawberry with Leaf Scorch",
+	"Tomato with Bacterial Spot",
+	"Tomato with Early Blight",
+	"Healthy Tomato",
+	"Late blight in Tomato",
+	"Tomato with Leaf Mold",
+	"Tomato with Septoria Leaf Spot",
+	"tomato with Spider Mites",
+	"Tomato with Target Spots",
+	"Tomato with Mosaic Virus",
+	"Yellow Leaf Curl in Tomato")
+'''
+
+disease_index_grape =(
+	"healthy Grapes",
+	"Isariopsis Leaf Spot in Grape",
+	"Black Measels in Grape",
+	"Black Rot in Grape",
+	"random background image"
+	)
+disease_index_apple =(
+	"Apple with Black Rot",
+	"Apple with Apple_scab ",
+	"healthy Apples",
+	"Apple with Cedar apple rust",
+	"random background image"
+	)
+
+disease_index_strawberry =(
+	"Healthy Strawberry",
+	"Strawberry with Leaf Scorch",
+	"random background image"
+	)
+
+disease_index_cherry =(
+	"healthy Cherry",
+	"Cherry with Powdery Mildew",
+	"random background image"
+	)
+
+disease_index_corn =(
+	"Corn with Cercospora leaf spot",
+	"healthy Corn",
+	"Northern Leaf Blight in Corn",
+	"Corn with Common rust",
+	"random background image"
+	)
+
+disease_index_potato =(
+	"Healthy Potato",
+	"Potato with Late Blight",
+	"Potato Early Blight",
+	"random background image"
+	)
+disease_index_peach =(
+	"healthy Peach",
+	"Bacterial Spot in Peach",
+	"random background image"
+	)
+
+disease_index_pepper =(
+	"Pepper with Bacterial Spot",
+	"healthy pepper",
+	"random background image"
+	)
+
+disease_index_tomato =(
+	"Apple with Black Rot",
+	"Apple with Apple_scab ",
+	"healthy Apples",
+	"Apple with Cedar apple rust",
+	"random background image"
+	)
+
+disease_index_other =(
+	"Apple with Black Rot",
+	"Apple with Apple_scab ",
+	"healthy Apples",
+	"Apple with Cedar apple rust",
+	"random background image"
+	)
+
+
+
+
+alldis=[{"1":"1"},
+{"2":"2"},
+{"3":"3"},
+{"4":"4"}]
